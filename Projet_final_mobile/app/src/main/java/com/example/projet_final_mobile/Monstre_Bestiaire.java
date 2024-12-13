ededcdf27b86e24ca0f0c40d4009c4e9db961bbc
Mathf32 <dionne.mathieu@outlook.com>
2024-12-13 16:05:33 -0500

    Bestiaire

--- Projet_final_mobile/app/src/main/java/com/example/projet_final_mobile/Monstre_Bestiaire.java
+++ Projet_final_mobile/app/src/main/java/com/example/projet_final_mobile/Monstre_Bestiaire.java
@@ -12,19 +12,29 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Monstre_Bestiaire extends AppCompatActivity {
-    List<Monster> les_Monstres = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        final List<Monster> les_Monstres = new ArrayList<>();
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.bestiaire);
-        new Thread(() -> {
-            les_Monstres = API_model.GetAllMonstre();
-            runOnUiThread(() -> {
-            });
-        }).start();
+        Thread t = new Thread(() -> {
+            List<Monster> m = API_model.GetAllMonstre();
+            synchronized (les_Monstres){
+                les_Monstres.clear();
+                les_Monstres.addAll(m);
+            }
+        });
+        t.start();
+        try {
+            t.join(); // Attendre que le thread termine son exécution
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+
+        Log.d("Monstre",les_Monstres.toString());
         RecyclerView recyclerView = findViewById(R.id.monster_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -40,7 +50,7 @@ public class Monstre_Bestiaire extends AppCompatActivity {
         monstreList adapter = new monstreList(les_Monstres, monster -> {
             // Action lorsqu'un monstre est cliqué
             // Exemple : changer l'image et afficher des stats fictives
-            monsterImage.setImageResource(R.drawable.ic_launcher_foreground); // Remplacez par une image réelle
+            monsterImage.setImageResource(getResources().getIdentifier(monster.image, "drawable", getPackageName())); // Remplacez par une image réelle
             damageMin.setText("Dammage Min:" + monster.dammage_min);
             damageMax.setText("Dammage Max: " + monster.dammage_max);
             armor.setText("Armor: " + monster.armor);
